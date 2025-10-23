3d57caa39e5b0486fa11785ceee788094944000b
EduardoDoradea <00086224@uca.edu.sv>
2025-10-22 20:20:31 -0600

    feat: Agregando objetos para utilizar

---
+++ ServerBE/data/cuentasData.js
@@ -0,0 +1,88 @@
+//CREACION DE OBJETOS Y EXPORTACION DE CUENTAS PARA UTILIZARLOS EN 
+//LAS OTRAS CARPETAS PARA PODER HACER LOS ENDPOINTS
+export const cuentas = [
+    {
+        "_id": "68f9580d2b9507cb61805648",
+        "index": 0,
+        "guid": "5e55d973-3a0f-49ea-8431-b1c977302bd5",
+        "isActive": false,
+        "balance": "$1,540.02",
+        "picture": "http://placehold.it/32x32",
+        "age": 36,
+        "eyeColor": "green",
+        "name": "Vivian Carney",
+        "gender": "female"
+    },
+    {
+        "_id": "68f9580d181abc7b06cf93ad",
+        "index": 1,
+        "guid": "efc9c4c7-36f6-4788-a9de-ef62711ed8ca",
+        "isActive": true,
+        "balance": "$1,627.91",
+        "picture": "http://placehold.it/32x32",
+        "age": 40,
+        "eyeColor": "blue",
+        "name": "Karla Huber",
+        "gender": "female"
+    },
+    {
+        "_id": "68f9580dff96c05a7761929d",
+        "index": 2,
+        "guid": "fc81f6b3-3e59-4239-a795-8d0f19096439",
+        "isActive": false,
+        "balance": "$1,471.51",
+        "picture": "http://placehold.it/32x32",
+        "age": 36,
+        "eyeColor": "brown",
+        "name": "Alba Velasquez",
+        "gender": "female"
+    },
+    {
+        "_id": "68f9580dcf51987226452918",
+        "index": 3,
+        "guid": "01f50c4c-7e30-4440-9f90-18d62d38bf83",
+        "isActive": true,
+        "balance": "$2,245.62",
+        "picture": "http://placehold.it/32x32",
+        "age": 38,
+        "eyeColor": "blue",
+        "name": "Price Brooks",
+        "gender": "male"
+    },
+    {
+        "_id": "68f9580dfb4904b3f7629f1e",
+        "index": 4,
+        "guid": "538bafb8-8b72-4dad-9e8e-65422a0c5bec",
+        "isActive": true,
+        "balance": "$3,878.30",
+        "picture": "http://placehold.it/32x32",
+        "age": 23,
+        "eyeColor": "brown",
+        "name": "Le Pittman",
+        "gender": "male"
+    },
+    {
+        "_id": "68f9580de2625340cbe01926",
+        "index": 5,
+        "guid": "c89b2137-869a-47aa-ac1d-9a2fe9e76624",
+        "isActive": true,
+        "balance": "$1,068.67",
+        "picture": "http://placehold.it/32x32",
+        "age": 25,
+        "eyeColor": "brown",
+        "name": "Clements James",
+        "gender": "male"
+    },
+    {
+        "_id": "68f9580d56fb139c36f72106",
+        "index": 6,
+        "guid": "31cde0ab-6e45-4a60-8534-25f6f1fd5cf0",
+        "isActive": true,
+        "balance": "$1,817.05",
+        "picture": "http://placehold.it/32x32",
+        "age": 22,
+        "eyeColor": "green",
+        "name": "Yolanda Downs",
+        "gender": "female"
+    }
+]
